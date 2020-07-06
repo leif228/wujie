6450ea0f767dfcd06dec561a559c7c05992fb675
leif <123465714@qq.com>
2020-07-06 16:26:29 +0800

    子自动tcp连接

--- wujieweb/util/request.js
+++ wujieweb/util/request.js
@@ -1,4 +1,3 @@
-
 document.write('<script src="../../component/laydate/laydate.js"></script>');
 document.write('<script src="../../util/jquery-3.4.1.js"></script>');
 document.write('<script src="../../component/layer/layer.js"></script>');
@@ -15,99 +14,109 @@ const domain = "http://127.0.0.1:8888";
 
 const api = {
 	domain: domain,
-	deviceRegist(reqObj = {}){
-		reqObj.url = domain + "/api/deviceRegist";
+	deviceRegist(reqObj = {}) {
+		reqObj.url = domain + "/deviceRegist";
 		return yajax(reqObj);
 	},
-	getChildNodes(reqObj = {}){
-		reqObj.url = domain + "/api/getChildNodes";
+	tcpClientConnect(reqObj = {}) {
+		reqObj.url = "http://" + reqObj.data.currIp + ":8888" + "/tcpClientConnect";
 		return yajax(reqObj);
 	},
-	getTreeData(reqObj = {}){
-		reqObj.url = domain + "/api/getTreeData";
+	getChildNodes(reqObj = {}) {
+		reqObj.url = domain + "/getChildNodes";
 		return yajax(reqObj);
 	},
-	userRegist(reqObj = {}){
-		reqObj.url = domain + "/api/userRegist";
+	getTreeData(reqObj = {}) {
+		reqObj.url = domain + "/getTreeData";
+		return yajax(reqObj);
+	},
+	userRegist(reqObj = {}) {
+		reqObj.url = domain + "/userRegist";
 		return yajax(reqObj);
 	},
 	// 退出登录
-	logout(reqObj = {}){
+	logout(reqObj = {}) {
 		reqObj.url = domain + "/PcUser/removeSession";
 		return yajax(reqObj);
 	},
 	// 获取按钮权限
-	getMenus(reqObj = {}){
+	getMenus(reqObj = {}) {
 		reqObj.loading = false;
-		reqObj.url = domain + "/api/findUserAllPermis";
+		reqObj.url = domain + "/findUserAllPermis";
 		return yajax(reqObj);
 	},
 	// 获取系统信息
-	getSystemConfig(reqObj = {}){
+	getSystemConfig(reqObj = {}) {
 		reqObj.loading = false;
 		reqObj.url = domain + "/PcUser/getSystemConfig";
 		return yajax(reqObj);
 	},
 	// 获取服务分类信息
-	getVerifyCode(reqObj = {}){
+	getVerifyCode(reqObj = {}) {
 		reqObj.loading = false;
 		reqObj.url = domain + "/PcUser/getVerifyCode";
 		return yajax(reqObj);
 	},
 	// 登陆
-	login(reqObj = {}){
-		reqObj.url = domain + "/api/userLogin";
+	login(reqObj = {}) {
+		reqObj.url = domain + "/userLogin";
 		return yajax(reqObj);
 	},
 	// 获取左侧菜单
-	getLeftMenus(reqObj = {}){
-		reqObj.url = domain + "/api/findUserAllPermis";
+	getLeftMenus(reqObj = {}) {
+		reqObj.url = domain + "/findUserAllPermis";
 		return yajax(reqObj);
 	},
 	// 获取分页信息
-	getPageData(reqObj = {}){
+	getPageData(reqObj = {}) {
 		reqObj.url = domain + reqObj.url;
 		return yajax(reqObj);
 	},
 	// 保存或者新增数据
-	saveFrom(reqObj = {}){
+	saveFrom(reqObj = {}) {
 		reqObj.url = domain + reqObj.url;
 		return yajax(reqObj);
 	},
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
+
 	// 自定义行为
-	customAction(reqObj = {}){
+	customAction(reqObj = {}) {
 		reqObj.url = domain + reqObj.url;
 		return yajax(reqObj);
 	},
-	
+
 	// 获取所有的权限
-	getAllMenuList(reqObj = {}){
+	getAllMenuList(reqObj = {}) {
 		reqObj.loading = false;
 		reqObj.url = domain + "/PcMenu/findAllMenuList";
 		return yajax(reqObj);
 	},
-	
-	
-	
+
+
+
 };
 
 // 同步执行ajax
-function yajax({type = "POST", url, data = {}, timeout = 10000, loading = true}) {
+function yajax({
+	type = "POST",
+	url,
+	data = {},
+	timeout = 10000,
+	loading = true
+}) {
 	return new Promise((resolve, reject) => {
 		let layerIdx = null;
-		if(loading){
+		if (loading) {
 			layerIdx = layer.msg('加载中', {
-			  icon: 16
+				icon: 16
 			});
 		}
 		let token = localStorage.getItem("token");
@@ -119,33 +128,33 @@ function yajax({type = "POST", url, data = {}, timeout = 10000, loading = true})
 			// //dataType:'json',
 			// contentType:'application/json',
 			timeout: timeout,
-			xhrFields:{
-			    withCredentials : true // 设置运行跨域操作
+			xhrFields: {
+				withCredentials: true // 设置运行跨域操作
 			},
 			beforeSend: (XMLHttpRequest) => {
-			    XMLHttpRequest.setRequestHeader("Authorization", token);
+				XMLHttpRequest.setRequestHeader("Authorization", token);
 			},
 			success(res) {
 				layerIdx ? layer.close(layerIdx) : '';
-				if(res.code == 'S00015'){
+				if (res.code == 'S00015') {
 					// if(parent.location.pathname.indexOf("page/index/index.html") != 0){
 					// 	parent.location.href = '../login/login.html';
 					// 	return;
 					// }
 					window.location.href = '../login/login.html';
 				}
-				if(res.code == 304){
+				if (res.code == 304) {
 					resolve(res);
 					return;
 				}
-				if(res.code != 0){
+				if (res.code != 0) {
 					reject(res);
 					errMsg(res.msg || res.status);
 					return;
 				}
 				resolve(res);
 			},
-			error(XmlHttpRequest,textStatus, errorThrown){
+			error(XmlHttpRequest, textStatus, errorThrown) {
 				layerIdx ? layer.close(layerIdx) : '';
 				errMsg(textStatus);
 				reject(textStatus);
